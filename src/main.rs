079362315bda07b7da1f2403bf9f7b074fa88a9b
ercembu <ercembulut@gmail.com>
2024-03-03 09:20:58 +0100

    prover witnesses ready

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,9 @@ fn main() {
     let n = 4;
     let mu = 2;
 
-    let N = mu * n;
+    let N = m * n;
+
+    assert!(m % mu == 0);
 
     let mut rng = StdRng::from_entropy();
     let mut cr = arguers::CommonRef::new(n, rng);
@@ -29,7 +31,7 @@ fn main() {
     let deck: Vec<Scalar> = (0..N).map(|card| Scalar::from(card as u64))
                                     .collect();
     let deck_r: Vec<Scalar> = (0..N).map(|_| cr.rand_scalar()).collect();
-    let deck_ciphertext: Vec<Ciphertext> = deck.iter()
+    let C_deck: Vec<Ciphertext> = deck.iter()
                                                 .zip(deck_r)
                                                 .map(|(card, r)| cr.encrypt(&EGInp::Scal(card.clone()), 
                                                                             &r
@@ -39,7 +41,20 @@ fn main() {
 
     let permutation: Vec<usize> = cr.rand_perm(&(0..N).collect());
 
+    let rho: Vec<Scalar> = permutation.iter()
+                                            .map(|_| cr.rand_scalar())
+                                            .collect();
+
+
+    let C_permd: Vec<Ciphertext> = permutation.iter()
+                                                .map(|pi| C_deck[*pi].clone())
+                                                .collect();
     println!("{:?}", permutation);
+    println!("{:?}", C_deck);
+    println!("{:?}", C_permd);
+    let C_: Vec<Ciphertext> = rho.iter()
+                                    .map(|r| cr.encrypt(&EGInp::Scal(Scalar::from(1 as u128)), r))
+                                    .collect();
 
 
     //let q: RistrettoPoint = cr.
